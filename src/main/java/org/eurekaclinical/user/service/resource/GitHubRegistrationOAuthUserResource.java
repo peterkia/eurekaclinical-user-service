f8cd01fb7669fa1d139aee5cd95525e14bf3f87e
Andrew Post <arpost@emory.edu>
2018-02-16 16:56:42 -0500

    Merge.

--- src/main/java/org/eurekaclinical/user/service/resource/GitHubRegistrationOAuthUserResource.java
+++ src/main/java/org/eurekaclinical/user/service/resource/GitHubRegistrationOAuthUserResource.java
@@ -26,7 +26,6 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
-import org.eurekaclinical.scribeupext.profile.EurekaProfile;
 
 import org.eurekaclinical.scribeupext.profile.GitHubProfile;
 import org.eurekaclinical.scribeupext.provider.GitHubProvider;
